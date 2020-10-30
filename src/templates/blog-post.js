b6ef8d9c7894b689dba4a4c904fb86a1c149c021
Justin Gardner <the1jscott@gmail.com>
2020-10-29 20:40:00 -0500

    added Speed Dial buttons for social sharing

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
+import SpeedDial from "../components/speedDial/speedDial";
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
@@ -8,9 +9,10 @@ import SEO from "../components/seo"
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
   const siteTitle = data.site.siteMetadata?.title || `Title`
-  const { previous, next } = data
+
 
   return (
+
     <Layout location={location} title={siteTitle}>
       <SEO
         title={post.frontmatter.title}
@@ -29,40 +31,22 @@ const BlogPostTemplate = ({ data, location }) => {
           dangerouslySetInnerHTML={{ __html: post.html }}
           itemProp="articleBody"
         />
+        <SpeedDial />
         <hr />
-        <footer>
-          <Bio />
+          <footer>
+          <img
+                className='avatar__image'
+                src='/images/profile.1.jpeg'
+                alt='user avatar'
+                
+              />
+          <Bio />         
         </footer>
       </article>
-      <nav className="blog-post-nav">
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
     </Layout>
   )
 }
+
 
 export default BlogPostTemplate
 
