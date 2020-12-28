b15a8a7754c48a40689bb61be179cf53eb61abfa
Justin Gardner <the1jscott@gmail.com>
2020-12-28 14:32:18 -0600

    corrected graphql query and moved profile_image filepath

--- src/components/bio/bio.js
+++ src/components/bio/bio.js
@@ -5,7 +5,7 @@ import Image from "gatsby-image"
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
-      avatar: file(absolutePath: { regex: "\/images\/profile_image.final.jpeg" }) {
+      avatar: file(absolutePath: { regex: "/images/profile_image.final.jpeg" }) {
         childImageSharp {
           fixed(width: 50, height: 50, quality: 95) {
             ...GatsbyImageSharpFixed
@@ -46,7 +46,7 @@ const Bio = () => {
         <div>
           <img
               className='avatar__image'
-              src='/images/profile_image.final.JPG'
+              src='/images/profile_image.final.jpeg'
               alt='user avatar'
                 
           /><p>  
